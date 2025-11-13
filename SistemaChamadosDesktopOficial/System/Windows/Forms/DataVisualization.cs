38ab3e7db6d9096106ce82dc31c00a7d8f6e23ee
JunCangiano <129407744+JunCangiano@users.noreply.github.com>
2025-11-13 10:56:54 -0300

    Add files via upload

---
+++ SistemaChamadosDesktopOficial/System/Windows/Forms/DataVisualization.cs
@@ -0,0 +1,30 @@
+﻿namespace System.Windows.Forms
+{
+    internal class DataVisualization
+    {
+        internal class Charting
+        {
+            internal class ChartArea
+            {
+                public string Name { get; internal set; }
+            }
+
+            internal class Series
+            {
+            }
+
+            internal class Legend
+            {
+            }
+
+            internal class Chart
+            {
+                public Chart()
+                {
+                }
+
+                public object ChartAreas { get; internal set; }
+            }
+        }
+    }
+}
